abae26e49eed566cd59b377049df832027632283
fundon <cfddream@gmail.com>
2014-09-23 23:24:37 +0800

    add test case

---
+++ iputils_test.go
@@ -0,0 +1,17 @@
+package iputils
+
+import (
+  "testing"
+  "github.com/bmizerany/assert"
+)
+
+func TestIP2Long(t *testing.T) {
+  var ip uint32 = 2346851510
+  str := IP2Long("139.226.28.182")
+  assert.Equal(t, ip, str)
+}
+
+func TestLong2IP(t *testing.T) {
+  ip := Long2IP(134744072)
+  assert.Equal(t, ip, "8.8.8.8")
+}
