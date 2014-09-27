59d72d6f1fac37f79259ebcf8c2181a277bbf6fd
fundon <cfddream@gmail.com>
2014-09-27 23:34:54 +0800

    go fmt

--- iputils_test.go
+++ iputils_test.go
@@ -1,17 +1,17 @@
 package iputils
 
 import (
-  "testing"
-  "github.com/bmizerany/assert"
+	"github.com/bmizerany/assert"
+	"testing"
 )
 
 func TestIP2Long(t *testing.T) {
-  var ip uint32 = 2346851510
-  str := IP2Long("139.226.28.182")
-  assert.Equal(t, ip, str)
+	var ip uint32 = 2346851510
+	str := IP2Long("139.226.28.182")
+	assert.Equal(t, ip, str)
 }
 
 func TestLong2IP(t *testing.T) {
-  ip := Long2IP(134744072)
-  assert.Equal(t, ip, "8.8.8.8")
+	ip := Long2IP(134744072)
+	assert.Equal(t, ip, "8.8.8.8")
 }
